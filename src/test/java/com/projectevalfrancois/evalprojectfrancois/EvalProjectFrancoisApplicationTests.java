9ec8212d8f726ced28cbe3243ddb28d10cea3a3f
Francois-hardy <55779879+Francois-hardy@users.noreply.github.com>
2022-02-18 09:45:54 +0100

    initial commit

---
+++ src/test/java/com/projectevalfrancois/evalprojectfrancois/EvalProjectFrancoisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projectevalfrancois.evalprojectfrancois;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvalProjectFrancoisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
